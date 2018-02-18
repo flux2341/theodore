701a676d49eef2aadb2bfb5e29bf8f5b1ed4200b
flux2341 <flux2341@gmail.com>
2018-02-18 12:44:05 -0800

    differentiation between markdown and non-markdown files, next step

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const nunjucks = require('nunjucks');
+const path = require('path')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var app = express();
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+nunjucks.configure('', {
+    autoescape: true,
+    express: app
+});
+
+//app.get('/.*\.md$/', function(req, res) {
+app.get('/*.md', function(req, res) {
+    // // req.route.path)
+    // let rel_path = req.path;
+    // let extension = path.extname(rel_path);
+    // if (extension === '.md') {
+    //
+    // }
+    res.render('index.html', {message: 'is md'});
+});
+
+// app.get('*', function(req, res) {
+//     res.render('index.html', {message: req.path});
+// });
+
+app.use(express.static('writing'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
--- writing/sibling.md
+++ writing/sibling.md
@@ -1,6 +1,6 @@
 
 
-# Subling
+# Sibling
 
 
 
